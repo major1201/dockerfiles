e82eb1cdb7ee545553a01a8ec06425906de61e1f
major1201 <major1201@gmail.com>
2021-12-18 22:51:00 +0800

    feat: new image debian:buster-runit

---
+++ debian/runit/README.md
@@ -0,0 +1,13 @@
+## Description
+
+`runit` could be used to run runit in docker.
+
+## Images
+
+- major1201/debian:buster-runit
+
+## Example
+
+```bash
+docker run -d --name debian major1201/debian:buster-runit
+```
---
+++ debian/runit/assets/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+# 1. base
+if [[ "${REGION}" = "cn" ]]; then
+    sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+    sed -i 's|security.debian.org/debian-security|mirrors.ustc.edu.cn/debian-security|g' /etc/apt/sources.list
+fi
+
+apt update
+apt install gnupg2 -y
+apt install vim localepurge python curl lsof wget less telnet -y --yes
+
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i -e 's/# zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/' /etc/locale.gen
+
+locale-gen --purge en_US
+
+# set default editor to vim
+update-alternatives --set editor /usr/bin/vim.basic
+
+# disable ntp service
+update-rc.d -f ntp remove
+
+# 2. runit
+apt install runit -y
+apt install cron at unscd rsyslog -y
+
+# install services
+mv /build/services/* /etc/service
+
+# clean
+apt-get clean
+rm -rf /build
+rm -rf /tmp/* /var/tmp/*
+rm -rf /var/lib/apt/lists/*
+rm -f /etc/cron.daily/*
